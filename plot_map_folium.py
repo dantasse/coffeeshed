3c5b07e3e10677cb096b76f3188b40e122937377
Dan Tasse <dan.tasse@gmail.com>
2016-01-28 22:38:34 -0800

    now it plots a map with points for all the edges

--- plot_map_folium.py
+++ plot_map_folium.py
@@ -2,10 +2,16 @@
 
 # Uses the Folium library to plot a simple map.
 
-import folium, ujson
+import folium, ujson, argparse
 
-map_1 = folium.Map(location=[45.372, -121.6972])
-map_1.simple_marker([45.3288, -121.6625], popup='Mt. Hood Meadows')
-map_1.simple_marker([45.3311, -121.7113], popup='Timberline Lodge')
-map_1.create_map(path='output.html')
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--coffeeshops_file', default='yelp_results_sf_bbox.json')
+    parser.add_argument('--map_output_file', default='coffeemap.html')
+    args = parser.parse_args()
+
+    map_1 = folium.Map(location=[37.783333, -122.416667], zoom_start=13)
+    for shop in ujson.load(open(args.coffeeshops_file)):
+        map_1.simple_marker([shop['latitude'], shop['longitude']], popup=shop['name'])
+    map_1.create_map(path=args.map_output_file)
 
